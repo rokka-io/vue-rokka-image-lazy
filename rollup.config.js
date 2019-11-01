f72fd6276e6337ea83bbca5336f1cbe682bb740f
Christian Stocker <me@chregu.tv>
2019-11-01 13:21:55 +0100

    use only umd

--- rollup.config.js
+++ rollup.config.js
@@ -2,23 +2,21 @@ import vue from 'rollup-plugin-vue'
 import { terser } from 'rollup-plugin-terser'
 import resolve from 'rollup-plugin-node-resolve'
 
+const defaultOutput = {
+  file: 'dist/index.umd.min.js',
+  format: 'umd',
+  name: 'vueRokkaImageLazy',
+  exports: 'named',
+  globals: {
+    'vue-rokka-image': 'vueRokkaImage',
+    lozad: 'lozad',
+  },
+  sourcemap: true,
+}
+
 export default {
   input: 'src/index.js',
-  output: [
-    {
-      file: 'dist/index.umd.min.js',
-      format: 'umd',
-      name: 'vueRokkaImageLazy',
-      exports: 'named',
-      globals: {
-        'vue-rokka-image': 'vueRokkaImage',
-        lozad: 'lozad',
-      },
-      sourcemap: true,
-    },
-    { file: 'dist/index.esm.js', format: 'es' },
-    { file: 'dist/index.esm.min.js', format: 'es', sourcemap: true },
-  ],
+  output: [defaultOutput, { ...defaultOutput, file: 'dist/index.min.js' }],
   plugins: [
     vue(),
     resolve(),
