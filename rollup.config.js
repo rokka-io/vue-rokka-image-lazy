0b4d42c803c7f8c493d7d9444a6e1cffc5ca6391
Christian Stocker <me@chregu.tv>
2019-11-01 21:06:19 +0100

    include full.umd.min.js

--- rollup.config.js
+++ rollup.config.js
@@ -2,29 +2,35 @@ import vue from 'rollup-plugin-vue'
 import { terser } from 'rollup-plugin-terser'
 import resolve from 'rollup-plugin-node-resolve'
 
-export default {
-  input: 'src/index.js',
-  output: [
-    {
-      file: 'dist/index.umd.min.js',
-      format: 'umd',
-      name: 'vueRokkaImageLazy',
-      exports: 'named',
-      globals: {
-        'vue-rokka-image': 'vueRokkaImage',
-        lozad: 'lozad',
-      },
-      sourcemap: true,
-    },
-    { file: 'dist/index.esm.js', format: 'es' },
-  ],
-  plugins: [
-    vue(),
-    resolve(),
-    terser({
-      include: [/^.+\.min\.js$/],
-      sourcemap: true,
-    }),
-  ],
-  external: ['vue-rokka-image', 'lozad'],
+const commonUMDconfig = {
+  file: 'dist/index.umd.min.js',
+  format: 'umd',
+  name: 'vueRokkaImageLazy',
+  exports: 'named',
+  globals: {
+    'vue-rokka-image': 'vueRokkaImage',
+    lozad: 'lozad',
+  },
+  sourcemap: true,
 }
+const plugins = [
+  vue(),
+  resolve(),
+  terser({
+    include: [/^.+\.min\.js$/],
+    sourcemap: true,
+  }),
+]
+export default [
+  {
+    input: 'src/index.js',
+    output: { ...commonUMDconfig, file: 'dist/full.umd.min.js', globals: {} },
+    plugins: plugins,
+  },
+  {
+    input: 'src/index.js',
+    output: [commonUMDconfig, { file: 'dist/index.esm.js', format: 'es' }],
+    plugins: plugins,
+    external: ['vue-rokka-image', 'lozad'],
+  },
+]
